b89d1def942f3846e6174fb3e0a317015bab90c0
Akash  Jaladurgam <AkashJal@Akashs-MacBook-Air.local>
2016-10-26 03:08:54 +0530

    changed file names

---
+++ while_loop_practise_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+    int x=1;
+    int number;
+    while(x<=5){
+        cin >> number;
+        x++;
+    }
+    return 0;
+    
+}
